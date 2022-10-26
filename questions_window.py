f7957477c8e96f6a3810ff5a0bba88ab8740e805
SamThornborough <samuel.thornborough@student.onslow.school.nz>
2022-10-26 15:42:42 +1300

    Created most of the layout, will now work on the Question Vertical widget

--- questions_window.py
+++ questions_window.py
@@ -32,29 +32,66 @@ main_widget.setStyleSheet("background-color: red;") # Not Perma
 
 left_widget = QWidget()
 hbox.addWidget(left_widget)
-left_widget.setStyleSheet("background-color: orange;") # Not Perma
+left_widget.setStyleSheet("background-color: orange;")  # Not Perma
+left_widget_vbox = QVBoxLayout()
+left_widget.setLayout(left_widget_vbox)
 
+player_options_widget = QListWidget()
+player_options_widget.addItem("1")
+player_score_widget = QLabel("Score")
+left_widget_vbox.addWidget(player_options_widget)
+left_widget_vbox.addWidget(player_score_widget)
+
+# Middle Widget
+
+current_held_skill_cards = QListWidget()
+for i in range(2):  # NOT PERMA, placeholder code
+    current_held_skill_cards.addItem(f"Card {i}")
+hbox.addWidget(current_held_skill_cards)
+current_held_skill_cards.setStyleSheet("background-color: cyan")
+
+# current_held_skill_cards = QListWidget()
+# current_held_skill_cards.addItem("Card 1")
+
+# # Right Widget
+
+# right_widget = QWidget()
+# right_widget_vbox_layout = QVBoxLayout()
+# right_widget.setLayout(right_widget_vbox_layout)
+# right_widget.setStyleSheet("background-color: purple;") # Not Perma
+
+# #          Right Widget text
+# hbox.addWidget(right_widget)
+
+# text_test = QLabel("Test the right widget")
+# right_widget_vbox_layout.addWidget(text_test)
+# text_test.setStyleSheet("background-color: green;") # Not Perma
+
+# #          Bottom Widget For Right
+
+# right_bottom_widget = QWidget()
+# right_widget_vbox_layout.addWidget(right_bottom_widget)
+# right_bottom_widget.setStyleSheet("background-color: white")
 
 # Right Widget
 
 right_widget = QWidget()
+hbox.addWidget(right_widget)
 right_widget_vbox_layout = QVBoxLayout()
 right_widget.setLayout(right_widget_vbox_layout)
-right_widget.setStyleSheet("background-color: purple;") # Not Perma
+right_widget.setStyleSheet("background-color: purple;")  # Not Perma
 
-#          Right Widget text
-hbox.addWidget(right_widget)
+#          Right Widget top
 
-text_test = QLabel("Test the right widget")
-right_widget_vbox_layout.addWidget(text_test)
-text_test.setStyleSheet("background-color: green;") # Not Perma
+player_go_button = QPushButton("GO")
+right_widget_vbox_layout.addWidget(player_go_button)
+player_go_button.setStyleSheet("background-color: green;")  # Not Perma
 
-#          Bottom Widget For Right
+#          Right Widget bottom
 
-right_bottom_widget = QWidget()
-right_widget_vbox_layout.addWidget(right_bottom_widget)
-right_bottom_widget.setStyleSheet("background-color: white")
-
+player_random_skill_card = QPushButton("SPIN FOR SKILL CARDS")
+right_widget_vbox_layout.addWidget(player_random_skill_card)
+player_random_skill_card.setStyleSheet("background-color: white")  # Not Perma
 
 main_window.show()
 app.exec()
